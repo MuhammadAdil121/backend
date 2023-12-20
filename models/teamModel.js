630fe0f5fb79d06a46459cbf08271cf1bb8d7bf4
MuhammadAdil121 <adilharoon304@gmail.com>
2023-12-20 19:05:27 +0500

    Work Updtaed

--- models/teamModel.js
+++ models/teamModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: '/users' }],
+  members: [{ type: mongoose.Schema.Types.ObjectId , ref: '/users' }],
 });
 
 const Team = mongoose.model('Team', teamSchema);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const AuthController = require('../controller/authController')
 const route = express.Router()
 
-route.get('/',AuthController.AdminProtected,AuthController.getUsers)
+route.get('/',AuthController.getUsers)
 route.post('/login', AuthController.login)
 route.post('/signUp', AuthController.signUp)
 route.get('/checkAuth' , AuthController.CheckAuth)
